8a877259aa8989d67119e3d429d80883ffe0d211
brantxiong <hustxiongbo@gmail.com>
2015-01-19 23:53:27 +0800

    add request post

--- python/post_file.py
+++ python/post_file.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+import requests
 
+response = requests.post(upload_file_url,
+                         files={'filename': open(uploadfile,'rb'),
+                                'description': 'upload test'})
+print response.content
